7404130c8bcf46d0a24a221039c91adbd62419c7
Creeeeger <104427569+Creeeeger@users.noreply.github.com>
2024-11-27 17:09:22 +0000

    feat: discover additional symbols

--- src/main/java/org/crecker/Main_data_handler.java
+++ src/main/java/org/crecker/Main_data_handler.java
@@ -166,39 +166,30 @@ public class Main_data_handler {
     }
 
     public static void start_Hype_Mode(int tradeVolume, float hypeStrength) {
-        String[] stockSymbols = { //List of theoretical trade-able symbols
-                "AEM", "CVNA", "VRT", "ODFL", "EW", "KHC", "VLO", "CTVA", "HES", "TCOM",
-                "MCHP", "LNG", "CBRE", "GLW", "FERG", "ACGL", "SNOW", "IT", "LVS", "DDOG",
-                "LULU", "AME", "DFS", "EA", "GIS", "YUM", "IRM", "VRSK", "IDXX", "HSY",
-                "BKR", "SYY", "NGG", "BNS", "NXPI", "COF", "EPD", "WDAY", "RSG", "AJG",
-                "FTNT", "ADSK", "AFL", "PCG", "PSA", "DHI", "TTD", "SLB", "MET", "ROP",
-                "GM", "SE", "TSN", "MKC", "EXPE", "TRU", "MDB", "MT", "ZTO", "BBY",
-                "ARE", "OMC", "LPLA", "AER", "CLX", "PFG", "ULTA", "VRSN", "LUV", "DGX",
-                "APTV", "DKNG", "UTHR", "AKAM", "FTAI", "POOL", "MGM", "CAVA", "TTEK", "SWK",
-                "GGG", "DUOL", "BMRN", "XPO", "AES", "TXRH", "DOCU", "GMAB", "CF", "USFD",
-                "REG", "SWKS", "TFC", "TRV", "NSC", "ET", "OKE", "APP", "URI", "PSX",
-                "GWW", "O", "COIN", "AZO", "AAPL", "NVDA", "MSFT", "GOOGL", "AMZN", "META",
-                "TSM", "BRK/B", "TSLA", "LLY", "AVGO", "WMT", "JPM", "XOM", "UNH", "V",
-                "NVO", "ORCL", "MA", "HD", "PG", "COST", "JNJ", "ABBV", "BAC", "NFLX",
-                "KO", "CRM", "SAP", "ASML", "CVX", "MRK", "TMUS", "AMD", "BABA", "SMFG",
-                "TM", "NVS", "PEP", "AZN", "LIN", "WFC", "CSCO", "ADBE", "MCD", "TMO",
-                "PM", "ABT", "IBM", "NOW", "MS", "QCOM", "GE", "AXP", "CAT", "TXN",
-                "ISRG", "FMX", "DHR", "RY", "VZ", "DIS", "PDD", "NEE", "AMGN", "INTU",
-                "RTX", "HSBC", "GS", "UBER", "PFE", "HDB", "CMCSA", "T", "UL", "ARM",
-                "AMAT", "SPGI", "LOW", "TTE", "BLK", "BKNG", "PGR", "UNP", "SNY", "SYK",
-                "HON", "LMT", "SCHW", "TJX", "BSX", "KKR", "ANET", "VRTX", "C", "BX",
-                "MUFG", "COP", "RACE", "MU", "PANW", "NKE", "ADP", "UPS", "CB", "MDT",
-                "ADI", "FI", "BUD", "SBUX", "DE", "UBS", "GILD", "IBN", "MMC", "PLD",
-                "SONY", "BMY", "AMT", "REGN", "SHOP", "PLTR", "SO", "LRCX", "INTC", "TD",
-                "ELV", "ICE", "BA", "HCA", "MDLZ", "INFY", "SHW", "KLAC", "DUK", "RELX",
-                "SCCO", "TT", "PBR", "CI", "EQIX", "ABNB", "ENB", "MO", "PYPL", "MCO",
-                "CEG", "CTAS", "BP", "CMG", "GD", "WM", "APH", "RIO", "ZTS", "BN",
-                "APO", "CME", "AON", "PH", "GEV", "WELL", "CL", "GSK", "BTI", "MSI",
-                "SNPS", "ITW", "SPOT", "USB", "TDG", "NOC", "PNC", "TRI", "DEO", "CRWD",
-                "CNQ", "MAR", "EQNR", "ECL", "CP", "CVS", "MRVL", "MMM", "APD", "CNI",
-                "TGT", "ORLY", "CDNS", "BDX", "EOG", "BMO", "FCX", "CARR", "FDX", "MCK",
-                "JD", "CSX"
+        String[] stockSymbols = {
+                "1Q", "AAOI", "AAPL", "ABBV", "ABNB", "ABT", "ACGL", "ACHR", "ADBE", "ADI", "ADP", "ADSK", "AEM", "AER", "AES", "AFL", "AFRM", "AJG", "AKAM", "ALAB"
+                , "AMAT", "AMC", "AMD", "AME", "AMGN", "AMT", "AMZN", "ANET", "AON", "AOSL", "APD", "APH", "APLD", "APO", "APP", "APTV", "ARE", "ARM", "ARWR", "AS"
+                , "ASML", "ASPI", "ASTS", "AVGO", "AXP", "AZN", "AZO", "Al", "BA", "BABA", "BAC", "BBY", "BDX", "BE", "BKNG", "BKR", "BLK", "BMO", "BMRN", "BMY"
+                , "BN", "BNS", "BNTX", "BP", "BRK/B", "BSX", "BTDR", "BTI", "BUD", "BX", "C", "CARR", "CAT", "CAVA", "CB", "CBRE", "CDNS", "CEG", "CELH", "CF"
+                , "CI", "CIFR", "CLSK", "CLX", "CMCSA", "CME", "CMG", "CNI", "CNQ", "COF", "COHR", "COIN", "COP", "CORZ", "COST", "CP", "CRDO", "CRM", "CRWD"
+                , "CSCO", "CSX", "CTAS", "CTVA", "CVNA", "CVS", "DAVE", "DDOG", "DE", "DEO", "DFS", "DGX", "DHI", "DHR", "DIS", "DJT", "DKNG", "DOCU", "DUK"
+                , "DUOL", "DXYZ", "EA", "ECL", "ELF", "ELV", "ENB", "ENPH", "EOG", "EPD", "EQIX", "EQNR", "ET", "EW", "EXAS", "EXPE", "FCX", "FDX", "FERG", "FI"
+                , "FIVE", "FLNC", "FMX", "FN", "FSLR", "FTAI", "FTNT", "FUTU", "GD", "GE", "GEV", "GGG", "GILD", "GIS", "GLW", "GM", "GMAB", "GME", "GOOGL", "GS"
+                , "GSK", "GWW", "HCA", "HD", "HDB", "HES", "HIMS", "HON", "HOOD", "HSAI", "HSBC", "HSY", "HUT", "IBM", "IBN", "ICE", "IDXX", "IESC", "INFY", "INOD"
+                , "INSP", "INTC", "INTU", "IONQ", "IREN", "IRM", "ISRG", "IT", "ITW", "JD", "JOBY", "JPM", "KHC", "KKR", "KLAC", "KODK", "LCID", "LIN"
+                , "LKNC", "LLY", "LMND", "LMT", "LNG", "LNTH", "LOW", "LPLA", "LRCX", "LULU", "LUMN", "LUNR", "LUV", "LVS", "LX", "MA", "MAR", "MARA", "MBLY"
+                , "MCHP", "MCK", "MCO", "MDB", "MDGL", "MDLZ", "MDT", "MET", "META", "MGM", "MKC", "MMC", "MMM", "MO", "MPWR", "MRK", "MRNA", "MRVL", "MS", "MSFT"
+                , "MSI", "MSTR", "MT", "MU", "MUFG", "NFE", "NFLX", "NGG", "NIO", "NKE", "NNE", "NOC", "NOVA", "NOW", "NSC", "NVDA", "NVO", "NVS", "NXPI"
+                , "O", "ODFL", "OKE", "OKLO", "OMC", "OPEN", "ORCL", "ORLY", "PANW", "PBR", "PCG", "PDD", "PFG", "PGHL", "PGR", "PH", "PLD"
+                , "PLTR", "PLUG", "PM", "PNC", "POOL", "POWL", "PSA", "PSX", "PTON", "PYPL", "QBTS", "QCOM", "QUBT", "RACE", "RCAT", "RDDT", "REG", "REGN", "RELX", "RGTI"
+                , "RIO", "RIOT", "RIVN", "RKLB", "ROOT", "ROP", "RSG", "RTX", "RUN", "RXRX", "RY", "SAP", "SBUX", "SCCO", "SCHW", "SE", "SEDG", "SG", "SHOP", "SHW"
+                , "SLB", "SMCI", "SMFG", "SMLR", "SMR", "SMTC", "SNOW", "SNPS", "SNY", "SOFI", "SONY", "SOUN", "SPGI", "SPOT", "STRL", "SWK", "SWKS", "SYK", "SYM"
+                , "SYY", "TCOM", "TD", "TDG", "TEM", "TFC", "TGT", "TJX", "TM", "TMDX", "TMO", "TMUS", "TRI", "TRU", "TRV", "TSLA", "TSM", "TSN", "TT"
+                , "TTD", "TTE", "TTEK", "TXN", "TXRH", "U", "UBER", "UBS", "UL", "ULTA", "UNH", "UNP", "UPS", "UPST", "URI", "USB", "USFD", "UTHR", "V", "VKTX"
+                , "VLO", "VRSK", "VRSN", "VRT", "VRTX", "VST", "W", "WDAY", "WELL", "WFC", "WM", "WOLF", "WULF", "XOM", "XPEV", "XPO", "YUM", "ZETA"
+                , "ZIM", "ZTO", "ZTS", "ВТВТ"
         };
+
         logTextArea.append(String.format("Activating hype mode for auto Stock scanning, Settings: %s Volume, %s Hype, %s Stocks to scan\n", tradeVolume, hypeStrength, stockSymbols.length));
         logTextArea.setCaretPosition(logTextArea.getDocument().getLength());
 
