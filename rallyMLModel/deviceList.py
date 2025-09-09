f36333a889314bdf1ceaf968969cce2ce3a07dd0
Creeeeger <104427569+Creeeeger@users.noreply.github.com>
2025-09-09 21:48:49 +0200

    update spike data collection

--- rallyMLModel/deviceList.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf  # Import the TensorFlow library
-
-# List all physical devices (CPUs, GPUs, TPUs) available to TensorFlow on this machine
-for device in tf.config.list_physical_devices():
-    print(device)  # Print the device information, e.g. type and name
